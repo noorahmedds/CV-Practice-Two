3c1bfdc48cfc391f5545e7c641e80611d89f7fac
noorahmedds <noorahmedds@outlook.com>
2019-07-12 22:20:34 +0500

    Updates to lecture7

--- ShahAssign/Supplementary Code/Lecture 7/lec7.py
+++ ShahAssign/Supplementary Code/Lecture 7/lec7.py
@@ -108,5 +108,56 @@ def main():
     # cv.waitKey(0)
 
 
+def opticalFlow():
+    # Code adapted from the following link:
+    # https://docs.opencv.org/3.4/d4/dee/tutorial_optical_flow.html
+    # This is to test out the library itself. The lukas kanade implementation
+    # Also receives these features to track here. So its not looking at the whole frame. Its looking at very specific
+    # Portions on the frame
+
+    seq1 = cv.imread("rseq01.jpg")
+    seq2 = cv.imread("rseq02.jpg")
+
+    # params for ShiTomasi corner detection
+    feature_params = dict( maxCorners = 100,
+                        qualityLevel = 0.3,
+                        minDistance = 7,
+                        blockSize = 7 )
+
+    # Parameters for lucas kanade optical flow
+    lk_params = dict( winSize  = (15,15),
+                    maxLevel = 2,
+                    criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+
+    # Create some random colors
+    color = np.random.randint(0,255,(100,3))
+
+    # Take first frame and find corners in it
+    old_frame = seq1
+    old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
+    p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(old_frame)
+    
+
+    frame = seq2
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # calculate optical flow
+    p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    # Select good points
+    good_new = p1[st==1]
+    good_old = p0[st==1]
+    # draw the tracks
+    for i,(new,old) in enumerate(zip(good_new, good_old)):
+        a,b = new.ravel()
+        c,d = old.ravel()
+        mask = cv.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+        frame = cv.circle(frame,(a,b),5,color[i].tolist(),-1)
+    
+    img = cv.add(old_frame,mask)
+    cv.imshow('frame',img)
+    
+    cv.waitKey(0)
+
 if __name__ == "__main__":
-    main()
+    opticalFlow()
